d9d3505bf1e5442559ecd40cf916bde7620cdb32
AyikOzgur <aozgurayik@hotmail.com>
2024-11-23 22:00:48 +0100

    Both way communication works

--- src/MspParser.cpp
+++ src/MspParser.cpp
@@ -24,28 +24,88 @@ std::string MspParser::getVersion()
     return MSP_PARSER_VERSION;
 }
 
-bool MspParser::encode(uint8_t* data, size_t& size, MspCommand command, std::vector<uint32_t> arguments)
+bool MspParser::encode(uint8_t* data, size_t& size, MspCommand command, std::vector<int32_t> arguments)
 {
-    // Build MSP_ATTITUDE request
-    if (command == MspCommand::MSP_ATTITUDE)
+
+    size = 0;
+
+    switch (command)
+    {
+    case MspCommand::MSP_IDENT: [[fallthrough]];
+    case MspCommand::MSP_STATUS: [[fallthrough]];
+    case MspCommand::MSP_RAW_IMU: [[fallthrough]];
+    case MspCommand::MSP_SERVO: [[fallthrough]];
+    case MspCommand::MSP_MOTOR: [[fallthrough]];
+    case MspCommand::MSP_RC: [[fallthrough]];
+    case MspCommand::MSP_RAW_GPS: [[fallthrough]];
+    case MspCommand::MSP_COMP_GPS: [[fallthrough]];
+    case MspCommand::MSP_ATTITUDE: [[fallthrough]];
+    case MspCommand::MSP_ALTITUDE: [[fallthrough]];
+    case MspCommand::MSP_ANALOG: [[fallthrough]];
+    case MspCommand::MSP_RC_TUNING: [[fallthrough]];
+    case MspCommand::MSP_PID: [[fallthrough]];
+    case MspCommand::MSP_BOX: [[fallthrough]];
+    case MspCommand::MSP_MISC: [[fallthrough]];
+    case MspCommand::MSP_MOTOR_PINS: [[fallthrough]];
+    case MspCommand::MSP_BOXNAMES: [[fallthrough]];
+    case MspCommand::MSP_PIDNAMES: [[fallthrough]];
+    case MspCommand::MSP_WP: [[fallthrough]];
+    case MspCommand::MSP_BOXIDS: [[fallthrough]];
+    case MspCommand::MSP_SERVO_CONF: [[fallthrough]];
+    case MspCommand::MSP_ACC_CALIBRATION: [[fallthrough]];
+    case MspCommand::MSP_MAG_CALIBRATION: [[fallthrough]];
+    case MspCommand::MSP_RESET_CONF: [[fallthrough]];
+    case MspCommand::MSP_BIND: [[fallthrough]];
+    case MspCommand::MSP_EEPROM_WRITE:
     {
         data[0] = '$';
         data[1] = 'M';
         data[2] = '<'; // Because it is a request
 
-        data[3] = 0; // Because it is a request
+        data[3] = size;
 
         data[4] = static_cast<uint8_t>(command);
 
-        data[5] = crc(data + 3, 2);
+        data[size + 5] = crc(data + 3, 2 + size);
 
-        size = 6;
-
+        size += 6; // Final buffer size
+        break;
     }
-    return false;
+    case MspCommand::MSP_SET_MOTOR: [[fallthrough]];
+    case MspCommand::MSP_SET_RAW_RC:
+    {
+        data[0] = '$';
+        data[1] = 'M';
+        data[2] = '<'; // Because it is setting
+
+        size = 32; // 16 arguments * 2 bytes
+        data[3] = size;
+
+        data[4] = static_cast<uint8_t>(command);
+
+        for (size_t i = 0; i < 16; i++)
+        {
+            int16_t value = arguments[i];
+            data[5 + i * 2] = value & 0xFF;
+            data[6 + i * 2] = (value >> 8) & 0xFF;
+        }
+
+        data[size + 5] = crc(data + 3, 2 + size);
+
+        size += 6; // Final buffer size
+        break;
+    }
+    default:
+    {
+        // Rest of commands will be implemented in the future.
+        return false;
+    }
+    }
+
+    return true;
 }
 
-bool MspParser::decode(uint8_t nextByte, MspCommand& command, std::vector<uint32_t>& arguments)
+bool MspParser::decode(uint8_t nextByte, MspCommand& command, std::vector<float>& arguments)
 {
     // Shift internal buffer
     for (size_t i = 0; i < sizeof(m_internalBuffer) - 1; i++)
@@ -57,27 +117,45 @@ bool MspParser::decode(uint8_t nextByte, MspCommand& command, std::vector<uint32
     m_internalBuffer[sizeof(m_internalBuffer) - 1] = nextByte;
 
     // Check if we have a valid attitude response
-    if (m_internalBuffer[0] == '$' && m_internalBuffer[1] == 'M' && m_internalBuffer[2] == '>')
+    if (m_internalBuffer[0] != '$' || m_internalBuffer[1] != 'M' || m_internalBuffer[2] != '>')
     {
-        // Check the id
-        if (m_internalBuffer[4] == static_cast<uint8_t>(MspCommand::MSP_ATTITUDE))
-        {
-
-            int size = m_internalBuffer[3];
-
-            uint8_t crcReceived = m_internalBuffer[5 + size];
-
-            uint8_t crcCalculated = crc(m_internalBuffer + 3, size + 2);
-
-            if (crcReceived == crcCalculated)
-            {
-                float pitch = static_cast<int16_t>(m_internalBuffer[5] | (m_internalBuffer[6] << 8));
-                std::cout << "Pitch: " << float(pitch/10) << std::endl;
-
-                return true;
-            }
-        }
+        return false;
     }
 
-    return false;
+    int size = m_internalBuffer[3];
+    uint8_t crcReceived = m_internalBuffer[5 + size];
+    uint8_t crcCalculated = crc(m_internalBuffer + 3, size + 2);
+    if (crcReceived != crcCalculated)
+    {
+        return false;
+    }
+
+    switch (static_cast<MspCommand>(m_internalBuffer[4]))
+    {
+    case MspCommand::MSP_ATTITUDE:
+    {
+        command = MspCommand::MSP_ATTITUDE;
+        float pitch = static_cast<int16_t>(m_internalBuffer[5] | (m_internalBuffer[6] << 8)) / 10.0f;
+        float roll = static_cast<int16_t>(m_internalBuffer[7] | (m_internalBuffer[8] << 8)) / 10.0f;
+        float yaw = static_cast<int16_t>(m_internalBuffer[9] | (m_internalBuffer[10] << 8)) / 10.0f;
+        arguments.push_back(pitch);
+        arguments.push_back(roll);
+        arguments.push_back(yaw);
+        break;
+    }
+    case MspCommand::MSP_ALTITUDE:
+    {
+        command = MspCommand::MSP_ALTITUDE;
+        float altitude = static_cast<int32_t>(m_internalBuffer[5] | (m_internalBuffer[6] << 8) | (m_internalBuffer[7] << 16) | (m_internalBuffer[8] << 24)) / 100.0f;
+        arguments.push_back(altitude);
+        break;
+    }
+    default:
+    {
+        // Rest of commands will be implemented in the future.
+        return false;
+    }
+    }
+
+    return true;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -4,21 +4,26 @@
 #include "MspParserVersion.h"
 #include "SerialPort.h"
 
-#define portNum 7
-#define baudrate 115200
-#define timeoutMsec 0
-
-
 int main()
 {
     std::cout << "MspParser version: " << MspParser::getVersion() << std::endl;
 
     MspParser mspParser;
 
-    std::string serialPortName = "\\\\.\\COM" + std::to_string(portNum);
+    std::string serialPortName;
+    std::cout << "Enter serial port name (/dev/ttyUSB0 for linux , COM1 for windows): ";
+    std::cin >> serialPortName;
+
+#if defined(_WIN32)
+    serialPortName = "\\\\.\\" + serialPortName;
+#endif
+
+    int baudrate;
+    std::cout << "Enter baudrate: ";
+    std::cin >> baudrate;
 
     cr::clib::SerialPort serialPort;
-    if (!serialPort.open(serialPortName.c_str(), baudrate, timeoutMsec))
+    if (!serialPort.open(serialPortName.c_str(), baudrate, 0))
     {
         std::cout << "ERROR: Serial port could not open." << std::endl;
         return -1;
@@ -33,25 +38,67 @@ int main()
         uint8_t bufferWrite[1024]{0};   
         uint8_t bufferRead[1024]{0};
 
+        std::cout << "---------------------------------" << std::endl;
+        std::cout << "-1 Exit" << std::endl;
+        std::cout << static_cast<int>(MspCommand::MSP_ATTITUDE) << "  Get angles." << std::endl;
+        std::cout << static_cast<int>(MspCommand::MSP_ALTITUDE) << "  Get altitude" << std::endl;
+        std::cout << static_cast<int>(MspCommand::MSP_SET_RAW_RC) << "  Set RC chanlles" << std::endl;
+        
+        int option;
+        std::cout << "Select option: ";
+        std::cin >> option;
+
+        if (option == -1)
+        {
+            return 0;
+        }
+
+        MspCommand command = static_cast<MspCommand>(option);
+
+        std::vector<int32_t> arguments;
+        if (command == MspCommand::MSP_SET_RAW_RC)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                int value;
+                std::cout << "Enter value for channel " << i + 1 << ": ";
+                std::cin >> value;
+                arguments.push_back(value);
+            }
+        }
+
         size_t size = 0;
-        mspParser.encode(bufferWrite, size, MspCommand::MSP_ATTITUDE, std::vector<uint32_t>{});
+        if (!mspParser.encode(bufferWrite, size, command, arguments))
+        {
+            std::cout << "ERROR: Could not encode MSP command." << std::endl;
+            continue;
+        }
 
         if (!serialPort.write(bufferWrite, size))
         {
             std::cout << "ERROR: Could not write to serial port." << std::endl;
-            return -1;
+            continue;
         }
 
         int bytes = serialPort.read(bufferRead, sizeof(bufferRead));
-        
         for (int i = 0; i < bytes; i++)
         {
             MspCommand command;
-            std::vector<uint32_t> arguments;
-            mspParser.decode(bufferRead[i], command, arguments);
+            std::vector<float> arguments;
+            if (mspParser.decode(bufferRead[i], command, arguments))
+            {
+                if (command == MspCommand::MSP_ATTITUDE)
+                {
+                    std::cout << "ATTITUDE: " << arguments[0] << " " << arguments[1] << " " << arguments[2] << std::endl;
+                }
+                else if (command == MspCommand::MSP_ALTITUDE)
+                {
+                    std::cout << "ALTITUDE: " << arguments[0] << std::endl;
+                }
+            }
         }
 
-        //std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
     return 0;
 }
